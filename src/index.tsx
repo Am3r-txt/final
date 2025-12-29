7cec8f62172735bb529907b846b6ea456e81acc1
Am3r-txt <amer.sebastian2@gmail.com>
2025-12-29 00:09:11 -0300

    fix: add lucide-react and fix css path

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';     // Esta es la línea que conecta el diseño
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
