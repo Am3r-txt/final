962e650b6b5c7cef0da7dc898f3634efd5e73512
Am3r-txt <amer.sebastian2@gmail.com>
2025-12-29 00:19:21 -0300

    fix: correct import syntax in HabitLog and css path

--- src/pages/HabitLog.tsx
+++ src/pages/HabitLog.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useApp } from '../context/AppContext';
-import { ... } from "../types.ts";
+import { Habit } from "../types";
 import { Save, AlertCircle } from 'lucide-react';
 
 const HabitLog: React.FC = () => {
